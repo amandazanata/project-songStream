7245c2892916751772401a196d6773b29fd5f3b7
Amanda Zanata <amandazanata46@gmail.com>
2023-01-14 14:35:13 -0300

    ajuste requisito 2 não importei proptypes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Carregando from './Carregando';
 import { createUser } from '../services/userAPI';
 
@@ -74,4 +74,4 @@ Login.propTypes = {
   history: PropTypes.object,
 }.isRequired;
 
-export default withRouter(Login);
+export default Login;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-/* import { getUser } from '../services/userAPI'; */
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends React.Component {
-/*   state = {
+  state = {
     name: '',
     email: '',
     bio: '',
@@ -23,12 +25,33 @@ class Profile extends React.Component {
         carregando: false,
       });
     });
-  } */
+  }
 
   render() {
+    const { name, email, bio, img, carregando } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        <div>
+          {carregando ? <Carregando /> : (
+            <>
+              <div>
+                <img
+                  src={ img }
+                  data-testid="profile-image"
+                  alt=""
+                />
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+              <h2>Nome</h2>
+              <h3>{name}</h3>
+              <h2>Email</h2>
+              <h3>{email || 'Nenhum e-mail cadastrado'}</h3>
+              <h2>Descrição</h2>
+              <h3>{bio || 'Nenhuma descrição cadastrada'}</h3>
+            </>
+          )}
+        </div>
       </div>
     );
   }
